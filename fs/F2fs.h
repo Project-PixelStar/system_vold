cff0649ebb807529a0b3f80f7c88dc1cd67247ae
Tom Marshall <tdm@cyngn.com>
2015-11-05 11:20:54 -0800

    vold: Honor mount options for ext4/f2fs partitions
     * Pass mount options and fstype to PublicVolume ctor
     * Pass mount options to ext4::Mount/f2fs:Mount
     * Use specified/default fstype if blkid fails
    
    This is trivially extensible for other fs types.
    
    [mikeioannina] Adapt for Pie and Q
    
    Change-Id: Ie5dd27f26a4a7129dd9efe6a7ad0a589928282a0

--- fs/F2fs.h
+++ fs/F2fs.h
@@ -29,9 +29,8 @@ namespace f2fs {
 bool IsSupported();
 
 status_t Check(const std::string& source, bool trusted);
-status_t Mount(const std::string& source, const std::string& target);
-status_t Format(const std::string& source, const bool is_zoned,
-                const std::vector<std::string>& user_devices);
+status_t Mount(const std::string& source, const std::string& target, const std::string& opts = "");
+status_t Format(const std::string& source, const std::string& zoned_device = "");
 
 }  // namespace f2fs
 }  // namespace vold
